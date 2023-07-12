12377f931bdcaf024e060f7f42bd1058a7e61c7f
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-07-12 10:21:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  var data = {
-    username: username,
-    password: password
-  };
+var data = {
+  username: username,
+  password: password
+};
 
-  fetch("http://localhost:5000/auth", {
+fetch("http://localhost:5000/auth", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
   },
-  body: JSON.stringify(data)
+  body: {
+    username: data.username,
+    password: data.password
+  }
 })
-  .then(response => {
-    if (response.ok) {
-      // Login successful, redirect to another page
-      window.location.href = "main.html";
-    } else {
-      response.json().then(data => {
-        document.getElementById("message").innerHTML = data.message;
-      });
-    }
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
+.then(response => response.json())
+.then(responseData => {
+  if (responseData.message === "Authentication successful.") {
+    // Login successful, redirect to another page
+    window.location.href = "main.html";
+  } else {
+    document.getElementById("message").innerHTML = responseData.error;
+  }
+})
+.catch(error => {
+  console.error("Error:", error);
 });
